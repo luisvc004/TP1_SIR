d6ab1f1369de2b6cec5a49e6586b6cd9d9daaff3
luisvc004 <luisvcarmo@gmail.com>
2024-10-20 03:04:23 +0100

    annyang implementation

--- lastfmAPI.js
+++ lastfmAPI.js
@@ -15,10 +15,14 @@ function fetchLastFmData(artist) {
 function displayLastFmData(data) {
     if (data.artist) {
         const artist = data.artist;
+        const formattedBio = artist.bio.summary
+            .replace(/(\.)(?=[^\.]*\.)/, '$1<br><br>')
+            .replace(/(?:^|\s)([1-9]|1[0-9]|20)\.\s/g, '<br><br>$1. ')
+            .replace(/(Read more on Last\.fm)/, '<br><br><br>$1<br><br>');
         lastfmData.innerHTML = `
             <div>
                 <h3>${artist.name}</h3>
-                <p>${artist.bio.summary}</p>
+                <p>${formattedBio}</p>
                 <p>Popularity: ${artist.stats.listeners} listeners</p>
             </div>
         `;
--- script.js
+++ script.js
@@ -3,32 +3,68 @@ import { fetchLastFmData } from '../lastfmAPI.js';
 
 const searchBtn = document.getElementById('searchBtn');
 const searchTerm = document.getElementById('searchTerm');
-const spotifyData = document.getElementById('spotifyData');
-const lastfmData = document.getElementById('lastfmData');
-const audioPlayer = document.getElementById('audioPlayer');
-const lyricsContainer = document.getElementById('lyrics');
 const artistTitle = document.getElementById('artistTitle');
+
+let inactivityTimeout;
+
+function fetchArtistData(artist) {
+    artistTitle.innerText = `"${artist}" Most Popular Songs on Spotify`;
+    fetchSpotifyData(artist);
+    fetchLastFmData(artist);
+}
 
 searchBtn.addEventListener('click', () => {
     const artist = searchTerm.value.trim();
-    if (artist){
-        artistTitle.innerText = `"${artist}" Most Popular Songs on Spotify`;
-        fetchSpotifyData(artist);
-        fetchLastFmData(artist);
+    if (artist) {
+        fetchArtistData(artist);
+    } else {
+        alert('Please enter an artist\'s name.');
     }
-    else alert('Please enter an artist\'s name.');
 });
 
 searchTerm.addEventListener('keypress', (event) => {
     if (event.key === "Enter") searchBtn.click();
 });
 
-audioPlayer.addEventListener('ended', () => {
-    const allPlayButtons = document.querySelectorAll('.play-btn');
-    allPlayButtons.forEach(btn => {
-        btn.innerHTML = '<i class="fas fa-play"></i>';
-    });
-    lyricsContainer.innerHTML = '';
-});
+function startVoiceRecognition() {
+    if (annyang) {
+        const commands = {
+            'search *term': function(term) {
+                searchTerm.value = term;
+                fetchArtistData(term);
+            },
+        };
 
-//export { fetchLyrics };
+        annyang.addCommands(commands);
+        annyang.start();
+
+        searchTerm.value = "Listening...";
+        searchTerm.classList.add('listening');
+
+        annyang.addCallback('result', function(phrases) {
+            searchTerm.value = phrases[0];
+            fetchArtistData(phrases[0]);
+            resetInactivityTimeout();
+        });
+
+        annyang.addCallback('end', function() {
+            searchTerm.classList.remove('listening');
+        });
+
+        function resetInactivityTimeout() {
+            clearTimeout(inactivityTimeout);
+            inactivityTimeout = setTimeout(() => {
+                annyang.abort();
+                searchTerm.classList.remove('listening');
+            }, 3000);
+        }
+
+        resetInactivityTimeout();
+        annyang.addCallback('start', resetInactivityTimeout);
+
+    } else {
+        console.log('Annyang is not supported in this browser.');
+    }
+}
+
+document.getElementById('voiceBtn').addEventListener('click', startVoiceRecognition);
